0465227702204ef629b9910b734ce57acc27a555
debanjan <debanjan@itscoman.com>
2015-05-05 11:47:33 +0400

    Added mt test suites

--- tests/test.c
+++ tests/test.c
@@ -57,6 +57,15 @@ START_TEST ( llist_01_create_delete_lists )
 }
 END_TEST
 
+START_TEST ( llist_01_create_delete_lists_mt )
+{
+    llist listToTest = NULL;
+    listToTest = llist_create ( NULL, NULL, MT_SUPPORT_TRUE );
+    ck_assert_ptr_ne ( listToTest, NULL );
+    llist_destroy ( listToTest, false, NULL );
+}
+END_TEST
+
 START_TEST ( llist_02_add_nodes )
 {
     int retval;
@@ -100,8 +109,90 @@ START_TEST ( llist_02_add_nodes )
 }
 END_TEST
 
+START_TEST ( llist_02_add_nodes_mt )
+{
+    int retval;
+    llist listToTest = NULL;
+    llist_node retptr;
+    listToTest = llist_create ( NULL, NULL, MT_SUPPORT_FALSE );
+
+    // Insert a 5 nodes 1..5
+    retval = llist_add_node ( listToTest, ( llist_node ) 1, ADD_NODE_FRONT );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    retval = llist_add_node ( listToTest, ( llist_node ) 2, ADD_NODE_FRONT );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    retval = llist_add_node ( listToTest, ( llist_node ) 3, ADD_NODE_FRONT );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    retval = llist_add_node ( listToTest, ( llist_node ) 4, ADD_NODE_FRONT );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    retval = llist_add_node ( listToTest, ( llist_node ) 5, ADD_NODE_FRONT );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    printf ( "List after adding nodes: " );
+    print_llist ( listToTest );
+
+    // Find that specific node, this should fail because no equal function was provided
+    retval = llist_find_node ( listToTest, ( llist_node ) 1, &retptr, NULL );
+    ck_assert_int_eq ( retval, LLIST_EQUAL_MISSING );
+
+    // find again, but this time, let's provide an equals function
+    retval = llist_find_node ( listToTest, ( llist_node ) 1, &retptr, trivial_equal );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+    ck_assert_ptr_eq ( retptr, ( llist_node ) 1 );
+
+    // find again, but this time, with a non existant node
+    retval = llist_find_node ( listToTest, ( llist_node ) 6, &retptr, trivial_equal );
+    ck_assert_int_eq ( retval, LLIST_NODE_NOT_FOUND );
+
+    llist_destroy ( listToTest, false, NULL );
+}
+END_TEST
 
 START_TEST ( llist_03_add_dynamic_nodes )
+{
+    int retval;
+    int *data[5];
+
+    for ( int i = 0; i < 5; i++ )
+    {
+        data[i] = malloc ( sizeof ( int ) );
+    }
+
+    llist listToTest = NULL;
+    llist_node retptr;
+    listToTest = llist_create ( NULL, NULL, MT_SUPPORT_FALSE );
+
+    // Insert a 5 nodes 1..5
+    retval = llist_add_node ( listToTest, ( llist_node ) data[0], ADD_NODE_FRONT );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    retval = llist_add_node ( listToTest, ( llist_node ) data[1], ADD_NODE_REAR );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    retval = llist_add_node ( listToTest, ( llist_node ) data[2], ADD_NODE_FRONT );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    retval = llist_add_node ( listToTest, ( llist_node ) data[3], ADD_NODE_REAR );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    retval = llist_add_node ( listToTest, ( llist_node ) data[4], ADD_NODE_FRONT );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    // find node
+    retval = llist_find_node ( listToTest, ( llist_node ) data[2], &retptr, trivial_equal );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+    ck_assert_ptr_eq ( retptr, ( llist_node ) data[2] );
+
+    // destroy list, and also free the data
+    llist_destroy ( listToTest, true, NULL );
+}
+END_TEST
+
+START_TEST ( llist_03_add_dynamic_nodes_mt )
 {
     int retval;
     int *data[5];
@@ -216,7 +307,111 @@ START_TEST ( llist_04_delete_nodes )
 END_TEST
 
 
+START_TEST ( llist_04_delete_nodes_mt )
+{
+    int retval;
+    llist_node temp;
+    llist listToTest = NULL;
+    listToTest = llist_create ( NULL, NULL, MT_SUPPORT_FALSE );
+
+    // Insert a 5 nodes 1..5
+    retval = llist_add_node ( listToTest, ( llist_node ) 1, ADD_NODE_FRONT );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    retval = llist_add_node ( listToTest, ( llist_node ) 2, ADD_NODE_FRONT );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    retval = llist_add_node ( listToTest, ( llist_node ) 3, ADD_NODE_FRONT );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    retval = llist_add_node ( listToTest, ( llist_node ) 4, ADD_NODE_FRONT );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    retval = llist_add_node ( listToTest, ( llist_node ) 5, ADD_NODE_FRONT );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    printf ( "List after adding nodes: " );
+    print_llist ( listToTest );
+
+    // This should fail because no equal function was given
+    retval = llist_delete_node ( listToTest, ( llist_node ) 1, NULL, false, NULL );
+    ck_assert_int_eq ( retval, LLIST_EQUAL_MISSING );
+
+    // Delete tail
+    retval = llist_delete_node ( listToTest, ( llist_node ) 1, trivial_equal, false, NULL );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    printf ( "List after deleting tail: " );
+    print_llist ( listToTest );
+
+    // Delete node in the middle
+    retval = llist_delete_node ( listToTest, ( llist_node ) 3, trivial_equal, false, NULL );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    printf ( "List after deleting middle node: " );
+    print_llist ( listToTest );
+
+    // Delete head
+    retval = llist_delete_node ( listToTest, ( llist_node ) 5, trivial_equal, false, NULL );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    printf ( "List after deleting head node: " );
+    print_llist ( listToTest );
+
+    // Delete a node that doesn't exist
+    retval = llist_delete_node ( listToTest, ( llist_node ) 6, trivial_equal, false, NULL );
+    ck_assert_int_eq ( retval, LLIST_NODE_NOT_FOUND );
+
+    retval = llist_delete_node ( listToTest, ( llist_node ) 2, trivial_equal, false, NULL );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    // The list should be empty now
+    temp =  llist_get_tail  ( listToTest );
+    ck_assert_ptr_ne ( temp, NULL );
+
+    // Delete last node
+    retval = llist_delete_node ( listToTest, ( llist_node ) 4, trivial_equal, false, NULL );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    // The list should be empty now
+    temp =  llist_get_tail  ( listToTest );
+    ck_assert_ptr_eq ( temp, NULL );
+
+    llist_destroy ( listToTest, false, NULL );
+}
+END_TEST
+
 START_TEST ( llist_05_list_for_each )
+{
+    int retval;
+    llist listToTest = NULL;
+    listToTest = llist_create ( NULL, NULL, MT_SUPPORT_FALSE );
+
+    // Insert a 5 nodes 1..5
+    retval = llist_add_node ( listToTest, ( llist_node ) 1, ADD_NODE_REAR );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    retval = llist_add_node ( listToTest, ( llist_node ) 2, ADD_NODE_REAR );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    retval = llist_add_node ( listToTest, ( llist_node ) 3, ADD_NODE_REAR );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    retval = llist_add_node ( listToTest, ( llist_node ) 4, ADD_NODE_REAR );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    retval = llist_add_node ( listToTest, ( llist_node ) 5, ADD_NODE_REAR );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    retval = llist_for_each ( listToTest, trivial_node_func );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    llist_destroy ( listToTest, false, NULL );
+}
+END_TEST
+
+
+START_TEST ( llist_05_list_for_each_mt )
 {
     int retval;
     llist listToTest = NULL;
@@ -327,6 +522,86 @@ START_TEST ( llist_06_insert_nodes )
 END_TEST
 
 
+START_TEST ( llist_06_insert_nodes_mt )
+{
+    int retval;
+    llist listToTest = NULL;
+    llist_node retptr;
+    listToTest = llist_create ( NULL, NULL, MT_SUPPORT_FALSE );
+
+    // Insert a 5 nodes 1..5
+    retval = llist_add_node ( listToTest, ( llist_node ) 1, ADD_NODE_FRONT );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    retval = llist_add_node ( listToTest, ( llist_node ) 2, ADD_NODE_FRONT );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    retval = llist_add_node ( listToTest, ( llist_node ) 3, ADD_NODE_FRONT );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    retval = llist_add_node ( listToTest, ( llist_node ) 4, ADD_NODE_FRONT );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    retval = llist_add_node ( listToTest, ( llist_node ) 5, ADD_NODE_FRONT );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    printf ( "List after adding nodes: " );
+    print_llist ( listToTest );
+
+    // Find the middle node (3)
+    retval = llist_find_node ( listToTest, ( llist_node ) 3, &retptr, trivial_equal );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    // Add node before
+    retval =  llist_insert_node ( listToTest,  ( llist_node ) 7, retptr, ADD_NODE_BEFORE );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    printf ( "List after adding before 3 node: " );
+    print_llist ( listToTest );
+
+
+    // Add node after
+    retval =  llist_insert_node ( listToTest,  ( llist_node ) 8, retptr, ADD_NODE_AFTER );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    printf ( "List after adding after 3 node: " );
+    print_llist ( listToTest );
+
+    // insert node at the start of the list (before the first node)
+    retval = llist_find_node ( listToTest, ( llist_node ) 5, &retptr, trivial_equal );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    retval =  llist_insert_node ( listToTest,  ( llist_node ) 9, retptr, ADD_NODE_BEFORE );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    printf ( "List after adding 9 before the first node: " );
+    print_llist ( listToTest );
+
+
+    // insert node at the start of the list (after the first node)
+    retval = llist_find_node ( listToTest, ( llist_node ) 9, &retptr, trivial_equal );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    retval =  llist_insert_node ( listToTest,  ( llist_node ) 10, retptr, ADD_NODE_AFTER );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    printf ( "List after adding 10 after the first node: " );
+    print_llist ( listToTest );
+
+    // insert node at the end of the list (after the first node)
+    retval = llist_find_node ( listToTest, ( llist_node ) 1, &retptr, trivial_equal );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    retval =  llist_insert_node ( listToTest,  ( llist_node ) 11, retptr, ADD_NODE_AFTER );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    printf ( "List after adding 11 after the last node: " );
+    print_llist ( listToTest );
+
+    llist_destroy ( listToTest, false, NULL );
+}
+END_TEST
+
 
 START_TEST ( llist_07_test_stack )
 {
@@ -359,7 +634,64 @@ START_TEST ( llist_07_test_stack )
 }
 END_TEST
 
+START_TEST ( llist_07_test_stack_mt )
+{
+    int retval;
+    llist listToTest = NULL;
+    llist_node retptr;
+    listToTest = llist_create ( NULL, NULL, MT_SUPPORT_FALSE );
+
+    // Push 1000 nodes
+    for ( unsigned long i = 0; i < 1000; i ++ )
+    {
+        retval = llist_push ( listToTest, ( llist_node ) i );
+        ck_assert_int_eq ( retval, LLIST_SUCCESS );
+    }
+
+    // Peek at the head
+    retptr = llist_peek ( listToTest );
+    ck_assert_int_eq ( ( unsigned long ) retptr, 999 );
+    for ( unsigned long i = 999; i > 0; i-- )
+    {
+        retptr = llist_pop ( listToTest );
+        ck_assert_int_eq ( ( unsigned long ) retptr, i );
+    }
+
+    printf ( "Count = %d\n", llist_size ( listToTest ) );
+    retptr = llist_pop ( listToTest );
+    ck_assert_ptr_eq ( retptr, NULL );
+
+    llist_destroy ( listToTest, false, NULL );
+}
+END_TEST
+
 START_TEST ( llist_08_list_reverse )
+{
+    int retval;
+    llist listToTest = NULL;
+    listToTest = llist_create ( NULL, NULL, MT_SUPPORT_FALSE );
+
+    // Insert a 5 nodes 1..5
+    retval = llist_add_node ( listToTest, ( llist_node ) 1, ADD_NODE_REAR );
+    retval = llist_add_node ( listToTest, ( llist_node ) 2, ADD_NODE_REAR );
+    retval = llist_add_node ( listToTest, ( llist_node ) 3, ADD_NODE_REAR );
+    retval = llist_add_node ( listToTest, ( llist_node ) 4, ADD_NODE_REAR );
+    retval = llist_add_node ( listToTest, ( llist_node ) 5, ADD_NODE_REAR );
+
+    printf ( "List before reversing: " );
+    print_llist ( listToTest );
+
+    retval = llist_reverse ( listToTest );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    printf ( "List After reversing: " );
+    print_llist ( listToTest );
+
+    llist_destroy ( listToTest, false, NULL );
+}
+END_TEST
+
+START_TEST ( llist_08_list_reverse_mt )
 {
     int retval;
     llist listToTest = NULL;
@@ -417,6 +749,38 @@ START_TEST ( llist_09_list_sort )
 }
 END_TEST
 
+START_TEST ( llist_09_list_sort_mt )
+{
+    int retval;
+    llist listToTest = NULL;
+    listToTest = llist_create ( trivial_comperator , NULL, MT_SUPPORT_FALSE );
+
+    // Insert a 5 nodes 1..5
+    retval = llist_add_node ( listToTest, ( llist_node ) 3, ADD_NODE_REAR );
+    retval = llist_add_node ( listToTest, ( llist_node ) 2, ADD_NODE_REAR );
+    retval = llist_add_node ( listToTest, ( llist_node ) 1, ADD_NODE_REAR );
+    retval = llist_add_node ( listToTest, ( llist_node ) 4, ADD_NODE_REAR );
+    retval = llist_add_node ( listToTest, ( llist_node ) 5, ADD_NODE_REAR );
+
+    printf ( "List before sorting: " );
+    print_llist ( listToTest );
+
+    retval = llist_sort ( listToTest, NULL, SORT_LIST_ASCENDING );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    printf ( "List After sorting ascending: " );
+    print_llist ( listToTest );
+
+    retval = llist_sort ( listToTest, NULL, SORT_LIST_DESCENDING );
+    ck_assert_int_eq ( retval, LLIST_SUCCESS );
+
+    printf ( "List After sorting descending: " );
+    print_llist ( listToTest );
+
+    llist_destroy ( listToTest, false, NULL );
+}
+END_TEST
+
 
 Suite *liblist_suite ( void )
 {
@@ -425,14 +789,23 @@ Suite *liblist_suite ( void )
     /* Core test case */
     TCase *tc_core = tcase_create ( "Core" );
     tcase_add_test ( tc_core, llist_01_create_delete_lists );
+    tcase_add_test ( tc_core, llist_01_create_delete_lists_mt );
     tcase_add_test ( tc_core, llist_02_add_nodes );
+    tcase_add_test ( tc_core, llist_02_add_nodes_mt );
     tcase_add_test ( tc_core, llist_03_add_dynamic_nodes );
+    tcase_add_test ( tc_core, llist_03_add_dynamic_nodes_mt );
     tcase_add_test ( tc_core, llist_04_delete_nodes );
+    tcase_add_test ( tc_core, llist_04_delete_nodes_mt );
     tcase_add_test ( tc_core, llist_05_list_for_each );
+    tcase_add_test ( tc_core, llist_05_list_for_each_mt );
     tcase_add_test ( tc_core, llist_06_insert_nodes );
+    tcase_add_test ( tc_core, llist_06_insert_nodes_mt );
     tcase_add_test ( tc_core, llist_07_test_stack );
+    tcase_add_test ( tc_core, llist_07_test_stack_mt );
     tcase_add_test ( tc_core, llist_08_list_reverse );
+    tcase_add_test ( tc_core, llist_08_list_reverse_mt );
     tcase_add_test ( tc_core, llist_09_list_sort );
+    tcase_add_test ( tc_core, llist_09_list_sort_mt );
 
     suite_add_tcase ( s, tc_core );
 
@@ -450,3 +823,6 @@ int main ( void )
     srunner_free ( sr );
     return ( number_failed == 0 ) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
+
+
+
